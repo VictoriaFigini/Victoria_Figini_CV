f809aa1e173686683c6f4a383d7bda76f1606e0f
VictoriaFigini <vicky.figini@gmail.com>
2022-12-29 20:09:07 -0300

    Button animations

---
+++ js/buttonanimation.js
@@ -0,0 +1,24 @@
+/* Settings Hover buttons */
+
+$(document).ready(function() {
+  $( "#settings a" ).each(function() {
+    var tipt = $(this).attr('title');
+    $(this).attr('title','');
+    $(this).append('<span>');
+    $(this).children('span').text(tipt);
+    var spanWidth = $(this).children('span').width() + 20;
+    var spanMarg = 0 - spanWidth/2;
+    $(this).children('span').css('width',spanWidth);
+    $(this).children('span').css('margin-left',spanMarg);
+    $(this).hover (
+      function() {
+        $(this).children('span').fadeIn();
+        $('#social a').css('opacity','.1');
+        $(this).css('opacity','1');
+      }, function() {
+        $(this).children('span').fadeOut();
+        $('#social a').css('opacity','1');
+      }
+    );
+  });                    
+});
